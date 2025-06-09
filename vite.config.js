ad9ed1671e8edc94a1eceff9680771ee10f311f9
Devesh Sawant <deveshsawant05@gmail.com>
2025-06-09 21:12:02 +0530

    Final commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/remix/vite"; 
 
 installGlobals({ nativeFetch: true });
 
@@ -62,7 +61,6 @@ export default defineConfig({
         v3_singleFetch: false,
         v3_routeConfig: true,
       },
-      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
